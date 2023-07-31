28785b6dbd12d39ef8a8979558c98e6ab3bef1c2
alekseev31446 <alekseev3446@gmail.com>
2023-07-31 13:02:19 +0300

    add level_2.apk

---
+++ myapplication1/build/generated/ksp/debug/java/com/example/myapplication1/Dao_Impl.java
@@ -0,0 +1,343 @@
+package com.example.myapplication1;
+
+import android.database.Cursor;
+import androidx.room.CoroutinesRoom;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Class;
+import java.lang.Exception;
+import java.lang.Integer;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import kotlinx.coroutines.flow.Flow;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class Dao_Impl implements Dao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<User> __insertionAdapterOfUser;
+
+  private final EntityDeletionOrUpdateAdapter<User> __deletionAdapterOfUser;
+
+  private final EntityDeletionOrUpdateAdapter<User> __updateAdapterOfUser;
+
+  public Dao_Impl(RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfUser = new EntityInsertionAdapter<User>(__db) {
+      @Override
+      public String createQuery() {
+        return "INSERT OR ABORT INTO `users` (`id`,`name`,`age`,`isStudent`) VALUES (?,?,?,?)";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, User value) {
+        if (value.getId() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindLong(1, value.getId());
+        }
+        if (value.getName() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getName());
+        }
+        stmt.bindLong(3, value.getAge());
+        final int _tmp = value.isStudent() ? 1 : 0;
+        stmt.bindLong(4, _tmp);
+      }
+    };
+    this.__deletionAdapterOfUser = new EntityDeletionOrUpdateAdapter<User>(__db) {
+      @Override
+      public String createQuery() {
+        return "DELETE FROM `users` WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, User value) {
+        if (value.getId() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindLong(1, value.getId());
+        }
+      }
+    };
+    this.__updateAdapterOfUser = new EntityDeletionOrUpdateAdapter<User>(__db) {
+      @Override
+      public String createQuery() {
+        return "UPDATE OR ABORT `users` SET `id` = ?,`name` = ?,`age` = ?,`isStudent` = ? WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, User value) {
+        if (value.getId() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindLong(1, value.getId());
+        }
+        if (value.getName() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getName());
+        }
+        stmt.bindLong(3, value.getAge());
+        final int _tmp = value.isStudent() ? 1 : 0;
+        stmt.bindLong(4, _tmp);
+        if (value.getId() == null) {
+          stmt.bindNull(5);
+        } else {
+          stmt.bindLong(5, value.getId());
+        }
+      }
+    };
+  }
+
+  @Override
+  public void insertUser(final User user) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __insertionAdapterOfUser.insert(user);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void deleteUser(final User user) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __deletionAdapterOfUser.handle(user);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void updateUser(final User user) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __updateAdapterOfUser.handle(user);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public Flow<List<User>> getAllUsers() {
+    final String _sql = "SELECT * FROM users";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return CoroutinesRoom.createFlow(__db, false, new String[]{"users"}, new Callable<List<User>>() {
+      @Override
+      public List<User> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "name");
+          final int _cursorIndexOfAge = CursorUtil.getColumnIndexOrThrow(_cursor, "age");
+          final int _cursorIndexOfIsStudent = CursorUtil.getColumnIndexOrThrow(_cursor, "isStudent");
+          final List<User> _result = new ArrayList<User>(_cursor.getCount());
+          while(_cursor.moveToNext()) {
+            final User _item;
+            final Integer _tmpId;
+            if (_cursor.isNull(_cursorIndexOfId)) {
+              _tmpId = null;
+            } else {
+              _tmpId = _cursor.getInt(_cursorIndexOfId);
+            }
+            final String _tmpName;
+            if (_cursor.isNull(_cursorIndexOfName)) {
+              _tmpName = null;
+            } else {
+              _tmpName = _cursor.getString(_cursorIndexOfName);
+            }
+            final int _tmpAge;
+            _tmpAge = _cursor.getInt(_cursorIndexOfAge);
+            final boolean _tmpIsStudent;
+            final int _tmp;
+            _tmp = _cursor.getInt(_cursorIndexOfIsStudent);
+            _tmpIsStudent = _tmp != 0;
+            _item = new User(_tmpId,_tmpName,_tmpAge,_tmpIsStudent);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  @Override
+  public Flow<List<User>> sortByName() {
+    final String _sql = "SELECT * FROM users ORDER BY name ASC";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return CoroutinesRoom.createFlow(__db, false, new String[]{"users"}, new Callable<List<User>>() {
+      @Override
+      public List<User> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "name");
+          final int _cursorIndexOfAge = CursorUtil.getColumnIndexOrThrow(_cursor, "age");
+          final int _cursorIndexOfIsStudent = CursorUtil.getColumnIndexOrThrow(_cursor, "isStudent");
+          final List<User> _result = new ArrayList<User>(_cursor.getCount());
+          while(_cursor.moveToNext()) {
+            final User _item;
+            final Integer _tmpId;
+            if (_cursor.isNull(_cursorIndexOfId)) {
+              _tmpId = null;
+            } else {
+              _tmpId = _cursor.getInt(_cursorIndexOfId);
+            }
+            final String _tmpName;
+            if (_cursor.isNull(_cursorIndexOfName)) {
+              _tmpName = null;
+            } else {
+              _tmpName = _cursor.getString(_cursorIndexOfName);
+            }
+            final int _tmpAge;
+            _tmpAge = _cursor.getInt(_cursorIndexOfAge);
+            final boolean _tmpIsStudent;
+            final int _tmp;
+            _tmp = _cursor.getInt(_cursorIndexOfIsStudent);
+            _tmpIsStudent = _tmp != 0;
+            _item = new User(_tmpId,_tmpName,_tmpAge,_tmpIsStudent);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  @Override
+  public Flow<List<User>> sortByAge() {
+    final String _sql = "SELECT * FROM users ORDER BY age ASC";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return CoroutinesRoom.createFlow(__db, false, new String[]{"users"}, new Callable<List<User>>() {
+      @Override
+      public List<User> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "name");
+          final int _cursorIndexOfAge = CursorUtil.getColumnIndexOrThrow(_cursor, "age");
+          final int _cursorIndexOfIsStudent = CursorUtil.getColumnIndexOrThrow(_cursor, "isStudent");
+          final List<User> _result = new ArrayList<User>(_cursor.getCount());
+          while(_cursor.moveToNext()) {
+            final User _item;
+            final Integer _tmpId;
+            if (_cursor.isNull(_cursorIndexOfId)) {
+              _tmpId = null;
+            } else {
+              _tmpId = _cursor.getInt(_cursorIndexOfId);
+            }
+            final String _tmpName;
+            if (_cursor.isNull(_cursorIndexOfName)) {
+              _tmpName = null;
+            } else {
+              _tmpName = _cursor.getString(_cursorIndexOfName);
+            }
+            final int _tmpAge;
+            _tmpAge = _cursor.getInt(_cursorIndexOfAge);
+            final boolean _tmpIsStudent;
+            final int _tmp;
+            _tmp = _cursor.getInt(_cursorIndexOfIsStudent);
+            _tmpIsStudent = _tmp != 0;
+            _item = new User(_tmpId,_tmpName,_tmpAge,_tmpIsStudent);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  @Override
+  public Flow<List<User>> sortByStatus() {
+    final String _sql = "SELECT * FROM users ORDER BY isStudent DESC";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return CoroutinesRoom.createFlow(__db, false, new String[]{"users"}, new Callable<List<User>>() {
+      @Override
+      public List<User> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "name");
+          final int _cursorIndexOfAge = CursorUtil.getColumnIndexOrThrow(_cursor, "age");
+          final int _cursorIndexOfIsStudent = CursorUtil.getColumnIndexOrThrow(_cursor, "isStudent");
+          final List<User> _result = new ArrayList<User>(_cursor.getCount());
+          while(_cursor.moveToNext()) {
+            final User _item;
+            final Integer _tmpId;
+            if (_cursor.isNull(_cursorIndexOfId)) {
+              _tmpId = null;
+            } else {
+              _tmpId = _cursor.getInt(_cursorIndexOfId);
+            }
+            final String _tmpName;
+            if (_cursor.isNull(_cursorIndexOfName)) {
+              _tmpName = null;
+            } else {
+              _tmpName = _cursor.getString(_cursorIndexOfName);
+            }
+            final int _tmpAge;
+            _tmpAge = _cursor.getInt(_cursorIndexOfAge);
+            final boolean _tmpIsStudent;
+            final int _tmp;
+            _tmp = _cursor.getInt(_cursorIndexOfIsStudent);
+            _tmpIsStudent = _tmp != 0;
+            _item = new User(_tmpId,_tmpName,_tmpAge,_tmpIsStudent);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
